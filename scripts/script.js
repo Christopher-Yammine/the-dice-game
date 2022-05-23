13ca8c61806f7048dbd38b3809eecf201e811bf8
ChristopherYammine <chrisyammine98@gmail.com>
2022-05-23 10:37:44 +0300

    Adding variables and creating a js file

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+
+    var firstDice= document.getElementsByClassName("img1")[0];
+    var secondDice=document.getElementsByClassName("img2")[0];
+    var dice1="./assets/dice1.png";
+    firstDice.src=dice1;
+    console.log(firstDice, "img1");
